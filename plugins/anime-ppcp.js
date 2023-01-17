7324c0fb502c71b49dc3596876b0c06d80d129d4
glospotnew <121785619+glospotnew@users.noreply.github.com>
2023-01-17 10:17:27 +0700

    Update anime-ppcp.js

--- plugins/anime-ppcp.js
+++ plugins/anime-ppcp.js
@@ -3,10 +3,10 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 let anu = await fetch('https://api.akuari.my.id/randomimage/ppcouple')
 let url = await anu.json()
 let { hasil } = url 
-let weem = 'PutraModz'
+let weem = '♾𝙶𝙻𝙾𝚂𝙿𝙾𝚃'
 m.reply(wait)
-await conn.sendButton(m.chat, 'Lanang', weem, hasil.cowok, [['⇄ Next ⇄', `${usedPrefix + command}`]], m)
-await conn.sendButton(m.chat, 'Woman', weem, hasil.cewek, [['⇄ Next ⇄', `${usedPrefix + command}`]], m)
+await conn.sendButton(m.chat, 'Cowok', weem, hasil.cowok, [['⇄ Next ⇄', `${usedPrefix + command}`]], m)
+await conn.sendButton(m.chat, 'Cewek', weem, hasil.cewek, [['⇄ Next ⇄', `${usedPrefix + command}`]], m)
 }
 handler.help = ['ppcp']
 handler.tags = ['internet']
