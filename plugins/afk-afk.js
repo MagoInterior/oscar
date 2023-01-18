3cb9f7104a46957f462b4be664c35e8b9cd02c63
glospotnew <121785619+glospotnew@users.noreply.github.com>
2023-01-18 21:21:52 +0700

    Update afk-afk.js

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -1,19 +1,14 @@
 let handler = async (m, { text }) => {
-    let user = global.db.data.users[m.sender]
-    user.afk = + new Date
-    user.afkReason = text
-    conn.sendHydrated(m.chat,
-`*––––––––【 AFK 】––––––––*`,
-`   • ᴀᴡᴀʏ ғʀᴏᴍ ᴋᴇʏʙᴏᴀʀᴅ sᴛᴀʀᴛᴇᴅ •
-
-${conn.getName(m.sender)} is now AFK
-
-ʀᴇᴀsᴏɴ: ${text ? '' + text : 'undefined'}`, null, null, null, null, null, [
-[`ʏɢ ᴀꜰᴋ ᴅɪ ʙᴜʟʟʏ`, `Ok`]
-], m)
+  let user = global.db.data.users[m.sender]
+  user.afk = + new Date
+  user.afkReason = text
+  m.reply(`
+${conn.getName(m.sender)} is now AFK${text ? ': ' + text : ''}
+`)
 }
-handler.help = ['afk [reason]']
-handler.tags = ['main']
+handler.help = ['afk [alasan]']
+handler.tags = ['group']
 handler.command = /^afk$/i
+handler.limit = false
 
-export default handler
+module.exports = handler
