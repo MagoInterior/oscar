e167c52337f5676fc497b51ab73b5611848cc968
glospotnew <121785619+glospotnew@users.noreply.github.com>
2023-01-20 06:52:41 +0700

    Update sticker-attp.js

--- plugins/sticker-attp.js
+++ plugins/sticker-attp.js
@@ -1,10 +1,15 @@
-let handler = async (m, { conn, text }) => {
-    let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
-    conn.sendFile(m.chat, global.API('xteam', '/attp', { file: '', text: teks }), 'attp.webp', '', m, false, { asSticker: true })
+import fetch from 'node-fetch'
+let handler = async (m, { conn, args }) => {
+   let response = args.join(' ').split('|')
+  if (!args[0]) throw 'ᴍᴀꜱᴜᴋᴋᴀɴ ᴛᴇxᴛ'
+  m.reply('ᴘʀᴏꜱᴇꜱ...')
+  let res = `https://saipulanuar.ga/api/maker/attp?apikey=APIKEY&text=bear=${response[0]}&apikey=Xynoz`
+  conn.sendFile(m.chat, res, 'xynz.webp', `ꜱᴜᴅᴀʜ ᴊᴀᴅɪ`, m, false)
 }
 handler.help = ['attp <teks>']
 handler.tags = ['sticker']
-
-handler.command = /^attp$/i
+handler.command = /^(attp)$/i
+handler.limit = true
+handler.premium = false
 
 export default handler
