47f02f41243fec033f0d171fe5becb1415c17707
glospotnew <121785619+glospotnew@users.noreply.github.com>
2023-01-19 21:32:33 +0700

    Update afk-afk.js

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -7,7 +7,7 @@ ${conn.getName(m.sender)} is now AFK${text ? ': ' + text : ''}
 `)
 }
 handler.help = ['afk [alasan]']
-handler.tags = ['group']
+handler.tags = ['main']
 handler.command = /^afk$/i
 handler.limit = false
 
