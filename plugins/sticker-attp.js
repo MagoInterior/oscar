6028097b7a7d6f7dd8a0ba126f3eba6ff334719b
glospotnew <121785619+glospotnew@users.noreply.github.com>
2023-01-23 23:02:37 +0700

    Update sticker-attp.js

--- plugins/sticker-attp.js
+++ plugins/sticker-attp.js
@@ -1,15 +1,12 @@
-import fetch from 'node-fetch'
-let handler = async (m, { conn, args }) => {
-   let response = args.join(' ').split('|')
-  if (!args[0]) throw 'ᴍᴀꜱᴜᴋᴋᴀɴ ᴛᴇxᴛ'
-  m.reply('ᴘʀᴏꜱᴇꜱ...')
-  let res = `https://saipulanuar.ga/api/maker/attp?apikey=APIKEY&text=bear=${response[0]}&apikey=Xynoz`
-  conn.sendFile(m.chat, res, 'xynz.webp', `ꜱᴜᴅᴀʜ ᴊᴀᴅɪ`, m, false)
+import { sticker } from '../lib/sticker.js'
+let handler = async (m, { conn, text }) => {
+    let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
+    let stiker = await sticker(null, global.API('https://api.erdwpe.com/api/maker/', 'attp?text', { file: '', text: teks }), global.packname, global.author)
+    if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
+    throw stiker.toString()
 }
 handler.help = ['attp <teks>']
 handler.tags = ['sticker']
-handler.command = /^(attp)$/i
+handler.command = /^attp$/i
 handler.limit = true
-handler.premium = false
-
-export default handler
+export default handler 
