bbee5e558e7b0c6b35b1810cea4c5a7832d51e16
glospotnew <121785619+glospotnew@users.noreply.github.com>
2023-02-05 09:42:06 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -15,7 +15,7 @@ conn.sendFile(m.chat, x.link, 'tiktok.mp4', cap, m)
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 handler.command = /^(tiktok|tt)$/i
-handler.limit = true
+handler.limit = false
 
 export default handler
 function ucapan() {
